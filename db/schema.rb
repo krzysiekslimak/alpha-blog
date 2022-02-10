381062c19a191d9d90c9d1cc195e6975ec978384
krzysiekslimak <krzysztof.slimak@outlook.com>
2022-02-10 09:35:32 +0100

    database change from SQLite3 to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_223022) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_06_223022) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "description"
   end
 
