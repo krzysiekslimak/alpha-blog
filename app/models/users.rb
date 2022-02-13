41b9e5c0a26583c89c85cf733e3c768f465576d4
krzysiekslimak <krzysztof.slimak@outlook.com>
2022-02-13 09:11:14 +0100

    User table added with validation

--- app/models/users.rb
+++
@@ -1,9 +0,0 @@
-class Users < ApplicationRecord
-    validates :username, presence: true,
-                        length: {minimum: 3, maximum: 100},
-                        uniqueness: { case_sensitive: false }
-    EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i               
-    validates :email, presence: true,
-                        length: {minimum: 5, maximum: 100},
-                        format: {with: EMAIL_REGEX}
-end
