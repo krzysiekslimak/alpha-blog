94efc465d66d1632f6e38bafbe1c269d4bb44ffc
krzysiekslimak <krzysztof.slimak@outlook.com>
2022-02-15 11:47:33 +0100

    new user form

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true,
     length: {minimum: 5, maximum: 100},
     format: {with: EMAIL_REGEX}
+has_secure_password
 end
