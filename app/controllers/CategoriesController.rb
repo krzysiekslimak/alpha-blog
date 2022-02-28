72385b1677a6f47bb8c0a65e956e97fc6f07e6b0
krzysiekslimak <krzysztof.slimak@outlook.com>
2022-02-28 20:37:13 +0100

    categories update

--- app/controllers/CategoriesController.rb
+++ app/controllers/CategoriesController.rb
@@ -1,11 +1,20 @@
 class CategoriesController < ApplicationController
+    
     def new
 
     end
+
+    def create
+
+
+    end
+
     def index
 
     end
+
     def show
         
     end
+
 end
